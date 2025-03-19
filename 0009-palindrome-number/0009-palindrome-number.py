b47b74a5f33f4aba94f929d963ef9c4155dc0d45
AMAIDI MOHAMED <amaidim30@gmail.com>
2025-03-18 17:14:06 -0700

    Time: 20 ms (19.54%), Space: 12 MB (99.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,3 +1,14 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
+class Solution(object):
+    def isPalindrome(self, x):
+        i=0
+        x=str(x)
+        if(int(x)<0):
+            return False
+        else:
+            while(i<len(x)/2 ):
+                if(x[i]!=x[len(x)-1-i]):
+                    return False
+                i=i+1
+        return True
+
         
