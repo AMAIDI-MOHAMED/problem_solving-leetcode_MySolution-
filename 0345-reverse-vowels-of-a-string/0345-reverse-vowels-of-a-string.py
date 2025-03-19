26940569e009f052211ecadbcafaa94c887fd71e
AMAIDI MOHAMED <amaidim30@gmail.com>
2025-03-19 07:59:34 -0700

    Time: 19 ms (30.95%), Space: 18.9 MB (18.8%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -5,7 +5,7 @@ class Solution:
         for c in s:
             if(c.lower() in ['a', 'e', 'i', 'o','u']):
                 voyelle.append(c)
-                nonV.append('car')
+                nonV.append('voy')
             else:
                 nonV.append(c)
         if(len(voyelle)==0):
@@ -14,7 +14,7 @@ class Solution:
         j=0
         s=""
         for i in range(len(nonV)):
-            if(nonV[i]=='car'):
+            if(nonV[i]=='voy'):
                 s+=voyelle[j]
                 j+=1
             else:
